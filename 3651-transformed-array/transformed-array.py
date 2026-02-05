6816c56f074c23e0c1c5165e2c2ce35d0b000599
Noor Nimrat Kaur <noornimrat00@gmail.com>
2026-02-05 11:02:07 +0530

    Time: 69 ms (9.87%) | Memory: 19.4 MB (10.76%) - LeetSync

--- 3651-transformed-array/transformed-array.py
+++ 3651-transformed-array/transformed-array.py
@@ -4,15 +4,7 @@ class Solution:
         result = [0] * n
 
         for index, num in enumerate(nums):
-            if num > 0:
-                right = num
-                newIndex = (index + right) % n
-                result[index] = nums[newIndex]
-            elif num < 0:
-                left = abs(num)
-                newIndex = (index - left) % n
-                result[index] = nums[newIndex]
-            else:
-                result[index] = num
+            newIndex = (index + num) % n
+            result[index] = nums[newIndex]
 
         return result
