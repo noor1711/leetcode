af7d7712c8e49d7e679d720304542369ed8bbac0
Noor Nimrat Kaur <noornimrat00@gmail.com>
2026-02-16 22:49:30 +0530

    Time: 3 ms (31.74%) | Memory: 19.5 MB (19.50%) - LeetSync

--- 360-sort-transformed-array/sort-transformed-array.py
+++ 360-sort-transformed-array/sort-transformed-array.py
@@ -1,4 +1,4 @@
 class Solution:
     def sortTransformedArray(self, nums: List[int], a: int, b: int, c: int) -> List[int]:
-        return list(sorted(map(lambda x: a * x**2 + b * x + c, nums)))
+        return sorted(map(lambda x: a * x**2 + b * x + c, nums))
         
