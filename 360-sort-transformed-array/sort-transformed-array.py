3a8a762d2158129ddc8f9bad841da098966fc668
Noor Nimrat Kaur <noornimrat00@gmail.com>
2026-02-17 14:27:19 +0530

    Time: 4 ms (8.47%) | Memory: 19.3 MB (73.35%) - LeetSync

--- 360-sort-transformed-array/sort-transformed-array.py
+++ 360-sort-transformed-array/sort-transformed-array.py
@@ -1,4 +1,38 @@
+
 class Solution:
+
     def sortTransformedArray(self, nums: List[int], a: int, b: int, c: int) -> List[int]:
-        return sorted(map(lambda x: a * x**2 + b * x + c, nums))
+        # okay so we are apply a quadratic function - U shape 
+        # instead of sorting again, we can do a two point approach 
+        start = 0
+        end = len(nums) - 1
+
+        def getQuadratic(x):
+            return a * pow(x, 2) + b * x + c
+
+        mappedArr = list(map(getQuadratic, nums))
+
+        # now we need to just merge the two lists 
+
+        ans = []
+        while start <= end:
+            qStart = mappedArr[start]
+            qEnd = mappedArr[end]
+
+            if a <= 0: # ends will be min, and center will be max
+                if qStart <= qEnd:
+                    ans.append(qStart)
+                    start += 1
+                else:
+                    ans.append(qEnd)
+                    end -= 1
+                continue
+
+            if qStart >= qEnd:
+                ans.append(qStart)
+                start += 1
+            else:
+                ans.append(qEnd)
+                end -= 1
         
+        return  ans if a <= 0 else ans[::-1]
